b22f42fdfcf8bde0aec3d2507b3ef18e868426c7
Glebkibenko <135252705+Glebkibenko@users.noreply.github.com>
2023-08-14 20:09:09 +0300

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,16 @@
+"use strict";
+let a = 1 ,
+b = 2;
+console.log('3')
+let c = 350 ,
+d = 5,
+e = 13;
+console.log('19775');
+let f = 11.1111,
+g = 52;
+console.log('(52 F-32)*5/9=11.1111)');
+//  prompt('Как тебя зовут?');
+// alert('Привет, Глеб');
+let hight = 10,
+widht = 20;
+console.log('10*20=200');
